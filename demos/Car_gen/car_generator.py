9a1462aa4da813963ef595df7082cc00a1234b71
BananaMan102911 <136490127+BananaMan102911@users.noreply.github.com>
2023-06-22 11:29:19 -0500

    Update car_generator.py

--- demos/Car_gen/car_generator.py
+++ demos/Car_gen/car_generator.py
@@ -1,8 +1,14 @@
-import Automobile
+import automobile
 
-#Create an automoile
-auto1 = Automobile.Automoblie ("Mecury", "Sable", "1234", 3.0, "Bob")
-auto2 = Automobile.Automoblie ("Honda", "Accord", "23456", 2.2, "Alice")
+#Create instances of automoile
+auto1 = automobile.Automoblie ("Mecury", "Sable", "1234", 3.0, "Bob", 2000)
+auto2 = automobile.Automoblie ("Honda", "Accord", "23456", 2.2, "Alice", 2003)
+
+
+#change auto 2 year
+auto2.__year = 2020
+#change auto1 owner
+auto1.set_owner("jerry")
 
 
 auto_list=[]
@@ -11,13 +17,6 @@ auto_list.append(auto2)
 
 #Print each automobiles info
 for auto in auto_list:
-    print(auto.make)
-    print(auto.model)
-    print("\n")
+    auto.print_data()
 
-
-print(auto1.make)
-print(auto1.model)
-print("\n")
-print(auto2.make)
-print(auto2.model)
+print(f"Auto1 is {auto1.get_age()} years old")
